0d1168cfb7f2ffb9496a45ca6220b87e4edfcccb
Ariel Honu <arielhonu@live.com>
2023-05-05 12:01:18 +0400

    Initial commit

---
+++ src/test/java/com/sefanebi/reactivespring/ReactivespringApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sefanebi.reactivespring;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivespringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
